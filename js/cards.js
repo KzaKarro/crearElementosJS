221d1d035ffecf3caa7a03290732789de99d6a18
Hernan <hernan.martinez@comunidad.unam.mx>
2025-03-24 10:11:17 -0600

    entregable

--- js/cards.js
+++ js/cards.js
@@ -1,6 +1,6 @@
 body = document.getElementById("mainBody");
 count =1;
-function createCard() {
+function createCard(innerText) {
     // <div class="col....">
     divCol = document.createElement("div");
     divCol.classList.add("col-12", "col-md-4", "col-xl-3");
@@ -16,23 +16,41 @@ function createCard() {
     // <h5 class="card-title">
     h5CardTitle= document.createElement("h5");
     h5CardTitle.classList.add("card-title");
-    h5CardTitle.innerText= count
+    h5CardTitle.innerText= innerText
 
     // <button class="btn btn-outline-success">
     buttonBtn = document.createElement("button");
     buttonBtn.classList.add("btn", "btn-outline-success");
     buttonBtn.innerText="Add new"
 
-    
+    // <label class="form-label">
+    labelFormLabel= document.createElement("label");
+    labelFormLabel.classList.add("form-label")
+    labelFormLabel.innerText="new item title" ;
+
+    //<input type="text" class="form-control mb-4">
+    inputFormControl=document.createElement("input");
+    inputFormControl.classList.add("form-control","mb-4")
+    inputFormControl.type="text";
 
     // ponemos etiquetas dentro de etiquetas
     divCardBody.appendChild(h5CardTitle);
     divCardBody.appendChild(buttonBtn);
+
+    divCardBody.appendChild(labelFormLabel);
+    divCardBody.appendChild(inputFormControl);
+
     divCard.appendChild(divCardBody);
     divCol.appendChild(divCard);
     body.appendChild(divCol);
 
     count ++;
-    buttonBtn.addEventListener("click",createCard);
+    buttonBtn.addEventListener("click",function (e,innerText=inputFormControl.value) {
+        createCard(innerText);
+        e.target.disabled=true;
+        e.target.classList.remove("btn-outline-success");
+        e.target.classList.add("btn-outline-danger")
+        e.target.innerText= "Clicked" ;
+    });
 }
 createCard();
